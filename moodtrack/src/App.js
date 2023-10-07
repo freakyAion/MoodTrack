2a8f1abb7d0f4b3314591dd13ed17a9e6db7ae24
freakyAion <FreakyAion@yandex.ru>
2023-10-07 22:21:48 +0500

    Test

--- moodtrack/src/App.js
+++ moodtrack/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,13 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+function App() {
+  return(
+    <h1>Test</h1>
+  );
+
 }
 
 export default App;
